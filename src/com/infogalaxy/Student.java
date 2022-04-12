c7c01d44edfccb5223022648878a8c41eabc0d7b
Prakash7277 <pkpatil.366@gmail.com>
2022-04-12 10:15:32 +0530

    ADDED:single Inheritance

---
+++ src/com/infogalaxy/Student.java
@@ -0,0 +1,36 @@
+package com.infogalaxy;
+
+import java.util.Scanner;
+
+public class Student {
+    Scanner sc = new Scanner(System.in);
+    int id;
+    String name;
+    void setStudent(){
+        System.out.println("Enter The ID : ");
+        id = sc.nextInt();
+        System.out.println("Enter The Name : ");
+        name = sc.next();
+    }
+}
+class Result extends Student{
+    int perc;
+    void setPerc() {
+        System.out.println("Enter The Percentage : ");
+        perc = sc.nextInt();
+    }
+    void getResult(){
+        System.out.println("ID : "+id);
+        System.out.println("Name : "+name);
+        System.out.println("Percentage : "+perc);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Result result = new Result();
+        result.setStudent();
+        result.setPerc();
+        result.getResult();
+    }
+}
+
